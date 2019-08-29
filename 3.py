18d1156b90cf587d31fafd65d05187aaf004d42c
Sanyab001 <sanyab001@gmail.com>
2019-08-30 01:09:40 +0300

    3

---
+++ 3.py
@@ -0,0 +1,20 @@
+# Простые делители числа 13195 - это 5, 7, 13 и 29.
+#
+# Каков самый большой делитель числа 600851475143, являющийся простым числом?
+
+import math
+
+
+def is_simple(number):
+    rez = math.ceil(math.sqrt(number))
+    l = []
+    for i in range(3, rez):
+        if number % i == 0:
+            if is_simple(i) == []:
+                l.append(i)
+    return l
+
+
+rez = is_simple(600851475143)
+print(max(rez))
+
